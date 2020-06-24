f695f03983ee8d7dd45bf57ec68da2f27f7f75d1
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2020-06-24 00:28:14 -0500

    css properties

--- tests/classStyleJit.php
+++ tests/classStyleJit.php
@@ -11,6 +11,10 @@ class ClassStyleJit extends TestCase
 	{
     $css = StyleJit\StyleJit::renderStyle('<a class="padding:1em">Save</a>');
     $this->assertEquals('.padding\\:1em{padding:1em}', $css);
+    $css = StyleJit\StyleJit::renderStyle('<a class="pad:1em">Save</a>');
+    $this->assertEquals('.pad\\:1em{padding:1em}', $css);
+    $css = StyleJit\StyleJit::renderStyle('<a class="p-lft:1em">Save</a>');
+    $this->assertEquals('.p-lft\\:1em{padding-left:1em}', $css);
 	}
 
 }
