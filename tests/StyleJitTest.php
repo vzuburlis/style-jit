c4f7ea89ed62d00ab7c0697cb98930101f48da17
Nikos Papagiannopoulos <panigrc@x-park.gr>
2020-07-05 14:50:59 +0200

    Fix errors and typos

--- tests/StyleJitTest.php
+++ tests/StyleJitTest.php
@@ -16,14 +16,17 @@ class StyleJitTest extends TestCase
     {
         $css = StyleJit::renderStyle('<a class="padding:1em">Save</a>');
         $this->assertEquals('.padding\\:1em{padding:1em}', $css);
+
         $css = StyleJit::renderStyle('<a class="pad:1em">Save</a>');
         $this->assertEquals('.pad\\:1em{padding:1em}', $css);
+
         $css = StyleJit::renderStyle('<a class="p-lft:1em">Save</a>');
         $this->assertEquals('.p-lft\\:1em{padding-left:1em}', $css);
+
         $css = StyleJit::renderStyle('<a class="p-l:1em">Save</a>');
         $this->assertEquals('.p-l\\:1em{padding-left:1em}', $css);
 
-        $css = StyleJit::renderStyle('<div class="dis:grid gr-t-c:1fr_1fr">Save</a>');
+        $css = StyleJit::renderStyle('<div class="dis:grid gr-t-c:1fr_1fr">Save</div>');
         $this->assertEquals('.dis\\:grid{display:grid}.gr-t-c\\:1fr_1fr{grid-template-columns:1fr 1fr}', $css);
     }
 }
