730ded85f5dd120c0d1d9bf92c8643091fdd4785
Nikos Papagiannopoulos <panigrc@x-park.gr>
2020-07-09 23:33:34 +0200

    Fix CS after solving conflicts

--- tests/StyleJitTest.php
+++ tests/StyleJitTest.php
@@ -10,31 +10,30 @@ use StyleJit\StyleJit;
  */
 class StyleJitTest extends TestCase
 {
-	public function testRenderStyle()
-	{
-    // double and single quotes
-    $css = StyleJit::renderStyle('<a class="padding:1em">Save</a>');
-    $this->assertEquals('.padding\\:1em{padding:1em}', $css);
+    public function testRenderStyle()
+    {
+        // double and single quotes
+        $css = StyleJit::renderStyle('<a class="padding:1em">Save</a>');
+        $this->assertEquals('.padding\\:1em{padding:1em}', $css);
 
-    $css = StyleJit::renderStyle('<a class="padding:1em">Save</a>');
-    $this->assertEquals('', $css); // existing class
+        $css = StyleJit::renderStyle('<a class="padding:1em">Save</a>');
+        $this->assertEquals('', $css); // existing class
 
-    $css = StyleJit::renderStyle("<a class='margin:1em'>Save</a>");
-    $this->assertEquals('.margin\\:1em{margin:1em}', $css);
+        $css = StyleJit::renderStyle("<a class='margin:1em'>Save</a>");
+        $this->assertEquals('.margin\\:1em{margin:1em}', $css);
 
-    // abbreviations
-    $css = StyleJit::renderStyle('<a class="pad:1em">Save</a>');
-    $this->assertEquals('.pad\\:1em{padding:1em}', $css);
+        // abbreviations
+        $css = StyleJit::renderStyle('<a class="pad:1em">Save</a>');
+        $this->assertEquals('.pad\\:1em{padding:1em}', $css);
 
-    $css = StyleJit::renderStyle('<a class="p-lft:1em">Save</a>');
-    $this->assertEquals('.p-lft\\:1em{padding-left:1em}', $css);
+        $css = StyleJit::renderStyle('<a class="p-lft:1em">Save</a>');
+        $this->assertEquals('.p-lft\\:1em{padding-left:1em}', $css);
 
-    $css = StyleJit::renderStyle('<a class="p-l:1em">Save</a>');
-    $this->assertEquals('.p-l\\:1em{padding-left:1em}', $css);
+        $css = StyleJit::renderStyle('<a class="p-l:1em">Save</a>');
+        $this->assertEquals('.p-l\\:1em{padding-left:1em}', $css);
 
-    // value attributes
-    $css = StyleJit::renderStyle('<div class="dis:grid gr-t-c:1fr_1fr">Save</a>');
-    $this->assertEquals('.dis\\:grid{display:grid}.gr-t-c\\:1fr_1fr{grid-template-columns:1fr 1fr}', $css);
-  }
-
+        // value attributes
+        $css = StyleJit::renderStyle('<div class="dis:grid gr-t-c:1fr_1fr">Save</a>');
+        $this->assertEquals('.dis\\:grid{display:grid}.gr-t-c\\:1fr_1fr{grid-template-columns:1fr 1fr}', $css);
+    }
 }
